dec5e73ab933547d5c59b02a9cb4040a79cdb0b5
lhk3337 <lhk3337@gmail.com>
2022-07-07 20:13:09 +0900

    [FIX] PostImage 컴포넌트 if문에서 삼항 연산자로 변경

--- components/Posts/PostImages.tsx
+++ components/Posts/PostImages.tsx
@@ -2,30 +2,30 @@ import { IProjectDataType, Itag, Iicon } from "api/apiType";
 import Image from "next/image";
 
 export default function PostImages(icon: Iicon) {
-  if (icon.type === "file") {
-    const { url } = icon.file;
-    return <Image src={url} width={30} height={30} alt="icon" />;
-  } else if (icon.type === "emoji") {
-    return <h1 className="text-2xl">{icon.emoji}</h1>;
-  } else if (icon.type === "external") {
-    const { url } = icon.external;
-    return <Image src={url} width={30} height={30} alt="icon" />;
-  } else {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-7 w-7"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={2}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-        />
-      </svg>
-    );
-  }
+  return (
+    <>
+      {icon.type === "file" ? (
+        <Image src={icon.file.url} width={30} height={30} alt="icon" />
+      ) : icon.type === "emoji" ? (
+        <h1 className="text-2xl">{icon.emoji}</h1>
+      ) : icon.type === "external" ? (
+        <Image src={icon.external.url} width={30} height={30} alt="icon" />
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-7 w-7"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+          />
+        </svg>
+      )}
+    </>
+  );
 }
