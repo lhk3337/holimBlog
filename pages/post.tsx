7121338cd24a7cc3d4761ebd8caf3cc1e0283fa7
lhk3337 <lhk3337@gmail.com>
2022-07-08 18:41:50 +0900

    [FEAT] post 갯수 추가

--- pages/post.tsx
+++ pages/post.tsx
@@ -8,6 +8,7 @@ export default function Post({ posts }: IProjectsDataType) {
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center p-5 mb-10">
+        <h2 className="mt-2 text-4xl font-bold">Total post : {posts.results.length}</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 w-full py-6">
           {posts.results.map((post: IProjectDataType) => (
             <PostItem key={post.id} {...post} />
