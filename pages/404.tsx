8326acbd8486e8c028da25b28bb7e6302f8ee90a
lhk3337 <lhk3337@gmail.com>
2022-07-09 20:32:45 +0900

    [FEAT] 404 error page 추가

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="fixed h-full flex bottom-0 top-0 left-0 right-0 items-center ">
+      <h1 className="text-3xl sm:text-5xl m-auto p-8 font-bold md:text-6xl 2xl:text-9xl">NOT FOUNDED!</h1>
+    </div>
+  );
+}
